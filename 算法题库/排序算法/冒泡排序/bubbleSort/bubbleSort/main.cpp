dd3da2b1db39d3c32e2f6b9797294d605ad9abf6
AugustJo <guangpuzhao@126.com>
2018-04-29 01:47:46 +0800

    增加冒泡排序算法工程

---
+++ 算法题库/排序算法/冒泡排序/bubbleSort/bubbleSort/main.cpp
@@ -0,0 +1,54 @@
+/**
+ * 对于一个int数组，请编写一个冒泡排序算法，对数组元素排序。
+ *
+ * 给定一个int数组A及数组的大小n，请返回排序后的数组。 
+ *
+ * 测试样例：
+ * [1,2,3,5,2,3],6
+ * [1,2,2,3,3,5]
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class BubbleSort {
+public:
+	int* bubbleSort(int* A, int n) {
+		if (A == NULL || n <= 1)
+			return A;
+		int tmp;
+		for (int i = n - 1; i > 0; --i) {
+			for (int j = 0; j < i; ++j) {
+				if (A[j] > A[j + 1]) {
+					tmp = A[j];
+					A[j] = A[j + 1];
+					A[j + 1] = tmp;
+				}
+			}
+		}
+
+		return A;
+	}
+};
+
+int main() {
+	int n;
+	cin >> n;
+	int *A = new int[n];
+	for (int i = 0; i < n; ++i) {
+		cin >> A[i];
+	}
+
+	BubbleSort bubble_sort;
+	bubble_sort.bubbleSort(A, n);
+
+	for (int i = 0; i < n; ++i) {
+		cout << A[i] << ' ';
+	}
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
