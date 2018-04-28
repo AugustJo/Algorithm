5e68fc252c007d622b355fd2f78927ccefb55e6f
AugustJo <guangpuzhao@126.com>
2018-04-29 02:31:35 +0800

    新增插入排序算法

---
+++ 算法题库/排序算法/插入排序/InsertionSort/main.cpp
@@ -0,0 +1,51 @@
+/**
+ *
+ * 对于一个int数组，请编写一个插入排序算法，对数组元素排序。
+
+ * 给定一个int数组A及数组的大小n，请返回排序后的数组。
+
+ * 测试样例：
+ * [1,2,3,5,2,3],6
+ * [1,2,2,3,3,5]
+ */
+#include <iostream>
+
+using namespace std;
+
+class InsertionSort {
+public:
+	int* insertionSort(int* A, int n) {
+		if (A == NULL || n <= 1)
+			return A;
+		int tmp;
+		for (int i = 0; i < n - 1; ++i) {
+			tmp = A[i + 1];
+			int j = i;
+			while (j > -1 && tmp < A[j]) {
+				A[j + 1] = A[j];
+				--j;
+			}
+			A[j + 1] = tmp;
+		}
+
+		return A;
+	}
+};
+
+int main() {
+	int n;
+	cin >> n;
+	int *A = new int[n];
+	for (int i = 0; i < n; ++i) {
+		cin >> A[i];
+	}
+	InsertionSort insert_sort;
+	insert_sort.insertionSort(A, n);
+	for (int i = 0; i < n; ++i) {
+		cout << A[i] << ' ';
+	}
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
