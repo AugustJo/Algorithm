a216a35c71ce67dac7cb438ef90b6d0e2de3abb0
AugustJo <guangpuzhao@126.com>
2018-04-29 01:52:20 +0800

    在冒泡排序中增加flag变量，减少循环次数，有一定优化效果

--- 算法题库/排序算法/冒泡排序/bubbleSort/bubbleSort/main.cpp
+++ 算法题库/排序算法/冒泡排序/bubbleSort/bubbleSort/main.cpp
@@ -18,10 +18,14 @@ public:
 	int* bubbleSort(int* A, int n) {
 		if (A == NULL || n <= 1)
 			return A;
+
+		int flag = 1;
 		int tmp;
-		for (int i = n - 1; i > 0; --i) {
+		for (int i = n - 1; i > 0 && flag == 1; --i) {
+			flag = 0;
 			for (int j = 0; j < i; ++j) {
 				if (A[j] > A[j + 1]) {
+					flag = 1;
 					tmp = A[j];
 					A[j] = A[j + 1];
 					A[j + 1] = tmp;
