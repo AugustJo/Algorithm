3a33f839a5bf41fe4394b37ae7950f9fd181a6ea
AugustJo <guangpuzhao@126.com>
2018-09-05 16:47:26 +0800

    带小数点的大数加减法

--- 算法题库/带小数点的大数加减/BigNumSumAndSub/main.cpp
+++ 算法题库/带小数点的大数加减/BigNumSumAndSub/main.cpp
@@ -6,10 +6,12 @@
 using namespace std;
 
 string sum(string, string, string, string);
+string sub(string, string, string, string);
 
 bool main()
 {
 	string num1, num2;
+	cout << "Input num1 , num2:" << endl;
 	cin >> num1 >> num2;
 	string num11, num12, num21, num22;
 	//输入检查
@@ -26,8 +28,7 @@ bool main()
 			}
 			else
 			{
-				cout << "false """ << endl;
-				system("pause");
+				cout << "num1: Please input correct form!!!" << endl;
 				return false;
 			}
 		}
@@ -42,9 +43,8 @@ bool main()
 			}
 			else
 			{
-				cout << "false """ << endl;
-				system("pause");
-				return false;
+				cout << "num2: Please input correct form!!!" << endl;
+				return 0;
 			}
 		}
 	}
@@ -68,7 +68,6 @@ bool main()
 		if (num12.empty())
 			num12 += '0';
 	}
-
 	else//如果是整数
 	{
 		num11 = num1;
@@ -98,10 +97,9 @@ bool main()
 		num22 += '0';
 	}
 
-	cout << "true " << sum(num11, num12, num21, num22) << endl;
-
+	cout << "The Sum result = " << sum(num11, num12, num21, num22) << endl;
+	cout << "The Sub result = " << sub(num11, num12, num21, num22) << endl;
 	system("pause");
-	return true;
 }
 
 
@@ -109,14 +107,11 @@ bool main()
 string sum(string strLong1, string strLong2, string strShort1, string strShort2)
 {
 	//小数部分计算
-	if (strLong2.size()<strShort2.size())
-	{
-		string strTemp = strLong2;
-		strLong2 = strShort2;
-		strShort2 = strTemp;
-	}
+	if (strLong2.size() < strShort2.size())
+		swap(strLong2, strShort2);
+
 	//补0
-	for (int i = strShort2.size(); i<strLong2.size(); ++i)
+	for (int i = strShort2.size(); i < strLong2.size(); ++i)
 		strShort2 += '0';
 	//反转字符串
 	reverse(strLong2.begin(), strLong2.end());
@@ -136,16 +131,12 @@ string sum(string strLong1, string strLong2, string strShort1, string strShort2)
 
 
 	//整数部分计算
-	if (strLong1.size()<strShort1.size())
-	{
-		string strTemp = strLong1;
-		strLong1 = strShort1;
-		strShort1 = strTemp;
-	}
+	if (strLong1.size() < strShort1.size())
+		swap(strLong1, strShort1);
+
 	//反转字符串
 	reverse(strLong1.begin(), strLong1.end());
 	reverse(strShort1.begin(), strShort1.end());
-
 
 	string strRes1(strLong1.size(), '0');
 	for (int i = 0; i != strShort1.size(); ++i)
@@ -169,8 +160,107 @@ string sum(string strLong1, string strLong2, string strShort1, string strShort2)
 	//反转回来
 	reverse(strRes1.begin(), strRes1.end());
 
+
 	//合并整数部分和小数部分
 	string strRes = strRes1 + '.' + strRes2;
 	return strRes;
 }
 
+//减法 被减数的整数部分 被减数的小数部分 减数的整数部分 减数的小数部分
+string sub(string strBjs1, string strBjs2, string strJs1, string strJs2)
+{
+	//小数部分进行减法计算
+	int cntTemp = strBjs2.size() - strJs2.size();
+	//补0
+	if (cntTemp <= 0)
+	{
+		for (int i = cntTemp; i != 0; ++i)
+		{
+			strBjs2 += '0';
+		}
+	}
+	else
+	{
+		for (int i = cntTemp; i != 0; --i)
+		{
+			strJs2 += '0';
+		}
+	}
+
+	//反转字符串
+	reverse(strBjs2.begin(), strBjs2.end());
+	reverse(strJs2.begin(), strJs2.end());
+	string strRes2(strBjs2.size(), '0');
+	int carry = 0;//进位
+	for (int i = 0; i != strBjs2.size(); ++i)
+	{
+		int a = strBjs2[i] - '0', b = strJs2[i] - '0';
+		a = a - b - carry;
+		if (a >= 0)
+		{
+			carry = 0;
+			strRes2[i] = a + '0';
+		}
+		else
+		{
+			carry = 1;
+			strRes2[i] = a + 10 + '0';
+		}
+	}
+	//反转回来
+	reverse(strRes2.begin(), strRes2.end());
+
+	//整数部分进行减法计算
+	//反转字符串
+	reverse(strBjs1.begin(), strBjs1.end());
+	reverse(strJs1.begin(), strJs1.end());
+	string strRes1(strBjs1.size(), '0');
+	for (int i = 0; i != strJs1.size(); ++i)
+	{
+		int a = strBjs1[i] - '0', b = strJs1[i] - '0';
+		a = a - b - carry;
+		if (a >= 0)
+		{
+			carry = 0;
+			strRes1[i] = a + '0';
+		}
+		else
+		{
+			carry = 1;
+			strRes1[i] = a + 10 + '0';
+		}
+	}
+	for (int i = strJs1.size(); i != strBjs1.size(); ++i)
+	{
+		int a = strBjs1[i] - '0';
+		a = a - carry;
+		if (a >= 0)
+		{
+			carry = 0;
+			strRes1[i] = a + '0';
+		}
+		else
+		{
+			carry = 1;
+			strRes1[i] = a + 10 + '0';
+		}
+	}
+	if (carry)//此时除数比被除数大，结果为负数
+	{
+		return "*";
+	}
+	//反转回来
+	reverse(strRes1.begin(), strRes1.end());
+
+	//清楚冗余0
+	string strTemp;
+	cntTemp = 0;
+	for (int i = 0; strRes1[i] == '0'; ++i)++cntTemp;
+	for (int i = cntTemp; i != strRes1.size(); ++i)
+		strTemp += strRes1[i];
+	strRes1 = strTemp;
+	//合并整数部分和小数部分
+	string strRes = strRes1 + '.' + strRes2;
+	//system("pause");
+	return strRes;
+}
